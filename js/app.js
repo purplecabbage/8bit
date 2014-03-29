13bed024c2fcd1212c49978d1c3fa263b6739e2e
Jesse MacFadyen <purplecabbage@gmail.com>
2014-03-28 18:47:27 -0700

    export image, and other goodness

--- js/app.js
+++ js/app.js
@@ -13,9 +13,10 @@
         var currentColor = "#FFF";
         var selectedTool;
 
-
+        var pixelData;
         var pxSz = 40;
-        var halfPixel = 0;//pxSz / 2;
+
+        var undoStack = [];
 
         exports.init = function()
         {
@@ -55,14 +56,60 @@
             context.fillStyle = clr || "#000";
             context.strokeStyle = "#333";
             context.beginPath();
-            for(var x =halfPixel; x < 480; x+=pxSz) {
-                for(var y = halfPixel;y < 640; y+= pxSz) {
-                    context.fillRect(x,y,pxSz,pxSz);
-                    context.strokeRect(x,y,pxSz,pxSz);
+            pixelData = [];
+
+            var pX = Math.ceil(480 / pxSz);
+            var pY = Math.ceil(640 / pxSz);
+
+            for (var x = 0; x < pX; x++) {
+                pixelData[x] = [];
+                for (var y = 0; y < pY; y++) {
+                    pixelData[x][y] = 0;//context.fillStyle;
+                    context.fillRect(x * pxSz, y * pxSz, pxSz, pxSz);
+                    context.strokeRect(x * pxSz, y * pxSz, pxSz, pxSz);
                 }
             }
             context.stroke();
             context.closePath();
+        }
+
+        function exportImage() {
+
+            var mux = 20;
+
+            var tempCanvas = document.createElement("canvas");
+            tempCanvas.width = pixelData.length * mux;
+            tempCanvas.height = pixelData[0].length * mux;
+// Not required to export data, but a HUD might be nice ...
+/*
+            tempCanvas.style.position = "absolute";
+            tempCanvas.style.right = "0px";
+            tempCanvas.style.top = "0px";
+            document.body.appendChild(tempCanvas);
+*/
+            var ctx = tempCanvas.getContext('2d');
+
+            // if we want a background color, it should go here ...
+            //ctx.clearRect(0, 0, pixelData.length * mux, pixelData[0].length * mux);
+
+            ctx.lineCap = "square";
+            ctx.lineJoin = "miter";
+            ctx.beginPath();
+            for (var x = 0; x < pixelData.length; x++) {
+                for (var y = 0; y < pixelData[x].length; y++) {
+                    
+                    if (pixelData[x][y] != 0) {
+                        ctx.fillStyle = pixelData[x][y];
+                        ctx.fillRect(x * mux, y * mux, mux, mux);
+                    }
+                }
+            }
+
+            ctx.closePath();
+
+            window.open(tempCanvas.toDataURL("image/png", ""));
+     
+
         }
         
         function onToolBtn(e)
@@ -80,7 +127,7 @@
                     selectedTool = e.target;
                     break;
                 case "toolBtnUndo" : 
-                    
+                    doUndoable();
                     break;
                 case "toolBtnColor" : 
                     if(e.target.active) {
@@ -90,8 +137,8 @@
                     }
                     showColorPicker(true);
                     break;
-                case "toolBtnExport" :
-                    console.log(currentCanvas.toDataURL("image/png", ""));
+                case "toolBtnExport":
+                    exportImage();
                     break;
                 case "toolBtnSettings": 
                     break;
@@ -186,15 +233,48 @@
             context.beginPath();
         }
 
+        function setPixelColor(x,y,clr,noUndo) {
+
+            if (!noUndo) {
+                undoStack.push({ x: x, y: y, clr: pixelData[x][y] });
+            }
+            pixelData[x][y] = clr;
+        }
+
+        function doUndoable() {
+            var obj = undoStack.pop();
+            if (obj) {
+
+                setPixelColor(obj.x, obj.y, obj.clr, true);
+                // store current context.color
+                var currentColor = context.fillStyle;
+                context.fillStyle = obj.clr || "#000";
+
+                context.beginPath();
+                context.fillRect(obj.x * pxSz, obj.y * pxSz, pxSz, pxSz);
+                context.strokeRect(obj.x * pxSz, obj.y * pxSz, pxSz, pxSz);
+                context.stroke();
+                context.closePath();
+                // restore current context.color
+                context.fillStyle = currentColor;
+
+                if (undoStack.length < 1) {
+                       // TODO: disable undo btn
+                }
+            }
+
+        }
+
         function onToolClick(e) {
             if(!wasMove) {
 
-                var x = Math.floor(( e.pageX - currentCanvas.offsetLeft ) / pxSz );// - e.currentTarget.offsetLeft;
-                var y = Math.floor(( e.pageY - currentCanvas.offsetTop ) / pxSz );// - e.currentTarget.offsetTop;
+                var x = Math.floor(( e.pageX - currentCanvas.offsetLeft ) / pxSz );
+                var y = Math.floor(( e.pageY - currentCanvas.offsetTop ) / pxSz );
 
                 context.beginPath();
-                context.fillRect(x * pxSz,y*pxSz,pxSz,pxSz);
-                context.strokeRect(x * pxSz,y*pxSz,pxSz,pxSz);
+                context.fillRect(x * pxSz, y * pxSz, pxSz, pxSz);
+                context.strokeRect(x * pxSz, y * pxSz, pxSz, pxSz);
+                setPixelColor(x, y, context.fillStyle);
                 context.stroke();
                 context.closePath();
             }
@@ -211,7 +291,8 @@
 
                 if(x != startX || y != startY) {
                     context.fillRect(x * pxSz,y*pxSz,pxSz,pxSz);
-                	context.strokeRect(x * pxSz,y*pxSz,pxSz,pxSz);
+                    context.strokeRect(x * pxSz, y * pxSz, pxSz, pxSz);
+                    setPixelColor(x, y, context.fillStyle);
                     wasMove = true;
                 }
 
